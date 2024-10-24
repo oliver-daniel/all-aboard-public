cd6bd9c654bccbbfbdfb7e9b7950e14ec6845437
Oliver Daniel <oliverdaniel00@gmail.com>
2024-10-24 15:35:23 -0400

    fix up styling for alert

--- src/components/Alert/index.tsx
+++ src/components/Alert/index.tsx
@@ -1,9 +1,11 @@
+import styles from "./styles.module.scss";
+
 type Props = {
   children: JSX.Element;
 };
 
 export const Alert = ({ children }: Props) => (
-  <div style={{ color: "red", border: "1px solid red", padding: "1rem" }}>
+  <div className={styles.alert} role="alert">
     {children}
   </div>
 );
